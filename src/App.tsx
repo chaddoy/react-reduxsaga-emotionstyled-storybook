e3bae69ff70df366d7189ea2cecba34559e9f510
chaddoy <capdumadag@gmail.com>
2025-04-30 21:48:15 +0800

    feat: install @emotion/styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import styled from '@emotion/styled';
+
+const SomeComp = styled.div({
+  color: 'hotpink',
+});
+
+const AnotherComp = styled.div`
+  color: ${(props) => props.color};
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,7 +25,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>
+        <AnotherComp color="green">Vite + React</AnotherComp>
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,10 +37,10 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        <SomeComp>Click on the Vite and React logos to learn more</SomeComp>
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
